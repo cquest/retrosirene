5c855083e74391b316a3c47ea9516d48699a88b4
cquest <cquest@openstreetmap.fr>
2019-08-02 14:37:56 +0200

    fix nomen_long pour entreprises individuelles

--- retrosirene.sql
+++ retrosirene.sql
@@ -60,7 +60,7 @@ SELECT
   null as prodet,
   null as prodpart,
   null as auxilt,
-  coalesce(regexp_replace(s.denominationunitelegale, s.nomunitelegale||'*'|| coalesce(s.nomusageunitelegale || '/','') || s.prenom1unitelegale||' '||coalesce(s.prenom2unitelegale,'')||' '||coalesce(s.prenom3unitelegale,'')||' '||coalesce(s.prenom4unitelegale,'')||' ','  +',' ')) as nomen_long,
+  coalesce(s.denominationunitelegale, trim(regexp_replace(s.nomunitelegale||'*'|| coalesce(s.nomusageunitelegale || '/','') || s.prenom1unitelegale||' '||coalesce(s.prenom2unitelegale,'')||' '||coalesce(s.prenom3unitelegale,'')||' '||coalesce(s.prenom4unitelegale,'')||' ','  +',' '))||'/') as nomen_long,
   s.sigleunitelegale as sigle,
   s.nomunitelegale as NOM,
   s.prenom1unitelegale as PRENOM,
